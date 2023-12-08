a0a31b32ce66c054e19148cb6c46568719e1b289
Maazalisaeed <maazali80009@gmail.com>
2023-12-08 19:03:34 +0500

    the start of search function development

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,32 +8,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-def search(request):
-    if request.method =="POST":
-        dict_to_store_entries = {}
-        search_query = request.POST.get('q', '')
-        search_query = search_query.lower()
-        All_entries = util.list_entries()
-        for entries in All_entries:
-            dict_to_store_entries[entries.lower()] = entries
-        redirect_url = dict_to_store_entries[search_query]
-        try:
-            return redirect(f"wiki/{redirect_url}")
-        except KeyError:
-            return HttpResponse("not found")
-                
-
-
-
-        #for entry in util.list_entries():
-            #if search_query == entry:
-               # return redirect(f"wiki/{search_query}")
-            #else:
-               # return HttpResponse("not found")
-        
-        
-    
-
+#def search(request):
+  
 def title(request, title):
     artical_name = util.get_entry(title)
     if artical_name == None:
