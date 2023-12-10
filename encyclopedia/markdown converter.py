3740c41489e7d0b155056c7efc54ce6283253e6d
Maazalisaeed <maazali80009@gmail.com>
2023-12-11 00:42:51 +0500

    going to rename markdown converter

--- encyclopedia/markdown converter.py
+++
@@ -1,8 +0,0 @@
-import re
-from util import get_entry
-
-def markdown_converter(title):
-    articel_name = get_entry(title)
-    heading = re.compile('#')
-    print(heading.sub('<h1>', articel_name))
-
