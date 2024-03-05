c8660478f164580e556bff73de38642499c30dbd
Maazalisaeed <maazali80009@gmail.com>
2024-03-05 20:05:06 +0500

    removed http responce form the improt sections

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse , redirect
+from django.shortcuts import render , redirect
 from django.urls import reverse
 from fuzzywuzzy import fuzz , process
 from . import util
