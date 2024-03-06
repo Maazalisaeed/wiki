08e0d2310c37f55c9da073db8745d0da868fd4ea
Maazalisaeed <maazali80009@gmail.com>
2024-03-06 12:43:26 +0500

    I thought I was do but I was so wrong there so many styling issues in this thing

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 # this will creat my coustom form for the edit and new pages with title being charater feild and content with a text are box
 class NewPageForm(forms.Form):
-    title = forms.CharField(label="Title",  max_length=200, required=True)
+    title = forms.CharField(label="Title", max_length=200, widget=forms.TextInput(attrs={'class':"round"}),required=True)
     content = forms.CharField(label="Content in Markdown", widget=forms.Textarea, required=True)
